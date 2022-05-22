ddf72cd5941896270edf66dcd230c45c7b0ab553
Nikita <cherenkov204@gmail.com>
2022-05-22 20:02:03 +0300

    tasks done

---
+++ task64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N в обратном порядке. M = 1; N = 5. -> «5, 4, 3, 2, 1»
+
+Console.WriteLine("Введите значение M и N через пробел");
+int[] entry = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Console.Write($"M = {entry[0]}; N = {entry[1]}. -> ");
+if (entry[0] >= entry[1]) Console.Write(Print(entry[0], entry[1]));
+if (entry[1] > entry[0]) Console.Write(Print(entry[1], entry[0]));
+
+string Print(int max, int min) {
+    string output = "«";
+    for (int i = max; i >= min; i--) {
+        output += $"{Convert.ToString(i)}";
+        if (i > min) output += ", ";
+    }
+    output += "»";
+    return output;
+}
---
+++ task66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. M = 1; N = 15 -> 120 M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите значение M и N через пробел");
+int[] entry = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Console.Write($"M = {entry[0]}; N = {entry[1]}. -> ");
+Console.Write(Calc(entry[0], entry[1]));
+
+int Calc(int a, int b) {
+    int c = 0;
+    int n = 0;
+    if (a > b) {
+        for (int i = b; i <= a; i++)
+        {
+            c += b + n;
+            n++;
+        }
+        return c;
+    } else
+    {
+        for (int i = a; i <= b; i++)
+        {
+            c += a + n;
+            n++;
+        }
+        return c;
+    }
+}
